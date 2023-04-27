ca9db0a239c09f4f65aa8dada14d615562a79b98
amandaduuaarte <amandaduuaarte@gmail.com>
2023-04-27 20:58:44 -0300

    fix: Adjusting home test file

--- tests/controllers/homeController.spec.js
+++ tests/controllers/homeController.spec.js
@@ -2,7 +2,7 @@ import homeController from '../../src/controllers/HomeController';
 
 describe('Home Controller test', () => {
   it('Should send 200 for successful response', () => {
-    // const sendSuccessResponse = homeController.index;
+    const sendSuccessResponse = homeController.index;
     // ToDo: Should send 200 for successful response when created new user
   });
 });
