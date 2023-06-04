f21086b2e2222fa3d5bf414e84f2b8c3ffa7d821
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-04 12:26:30 -0300

    feat: Create student and user model

---
+++ src/models/student.js
@@ -0,0 +1,20 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Student extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        last_name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        age: Sequelize.INTEGER,
+        weight: Sequelize.FLOAT,
+        height: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      },
+    );
+    return this;
+  }
+}
