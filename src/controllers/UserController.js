452ea9beef800ceb74fa03564bdb8fe4a7937e6a
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-04 16:40:02 -0300

    feat: Create users crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,11 +4,79 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.status(200).json(
+      return res.status(200).json(
         newUser,
       );
     } catch (err) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.status(200).json(users);
+    } catch {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.status(200).json(user);
+    } catch {
+      return res.json({
+        errors: ['User not found'],
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id not found'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const newUserData = await user.update(req.body);
+      return res.status(200).json(newUserData);
+    } catch {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id not found'],
+        });
+      }
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      await user.destroy();
+      return res.status(200);
+    } catch {
+      return res.status(400).json({
         errors: err.errors.map((e) => e.message),
       });
     }
--- tests/controllers/userController.spec.js
+++ tests/controllers/userController.spec.js
@@ -1,9 +1,20 @@
 import UserController from '../../src/controllers/UserController';
+import UserModel from '../../src/models/user';
 
 describe('User Controller test', () => {
-  it('Should create user and send 200 for successful response', async () => {
-    const createUser = await UserController.store();
+  test('Should create user with 200 status.', async () => {
+    const req = { body: { name: 'Exemplo', password: '123456', email: 'exemplo@example.com' } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-    expect(createUser).to.have.status(200);
+    await UserController.store(req, res);
+
+    // Verifique se o status foi definido corretamente
+    expect(res.status).toHaveBeenCalledWith(200);
+
+    // Verifique se o método json foi chamado com os dados do novo usuário
+    expect(res.json).toHaveBeenCalledWith(req.body);
   });
 });
