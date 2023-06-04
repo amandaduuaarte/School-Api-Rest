98a8b39a6acc042fc4ef17527a67399cf0916965
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-04 12:27:31 -0300

    feat: Create test config for new student

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,7 +1,17 @@
+import Student from '../models/student';
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const newStudent = Student.create({
+      name: 'Amanda',
+      last_name: 'Duarte',
+      email: 'amanda@gmail.com',
+      age: 21,
+      height: 1.72,
+      width: 68,
+    });
     res.status(200).json({
-      body: 'Home route',
+      body: newStudent,
     });
   }
 }
