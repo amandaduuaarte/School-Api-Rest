c3b37a6ea5b0ec061c1c42d26844474d071bde57
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-04 17:07:52 -0300

    feat: Configure token controller and routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import './src/database';
 import express from 'express';
 import homeRoutes from './src/routes/home';
 import userRoutes from './src/routes/user';
+import tokenRoutes from './src/routes/token';
 
 class App {
   constructor() {
@@ -23,6 +24,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -49,6 +49,11 @@ export default class User extends Model {
         user.password_hash = await bcryptjs.hash(user.password, 8);
       }
     });
+
     return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
   }
 }
