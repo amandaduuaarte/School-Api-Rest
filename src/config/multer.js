87b5d013429d21de82025f573e22a30dfd0f32a2
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-05 22:16:15 -0300

    feat: Add multer configs to send files

---
+++ src/config/multer.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+const random = () => {
+  Math.floor(Math.random() * 1000 + 1000);
+};
+export default {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${random()}${extname(file.originalname)}`);
+    },
+  }),
+};
