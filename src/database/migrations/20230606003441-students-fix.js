ff4367722e1cab1ff793855a00e3159bed1fef2c
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-05 21:49:32 -0300

    fix: Adjusting column names

--- src/database/migrations/20230606003441-students-fix.js
+++ src/database/migrations/20230606003441-students-fix.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
       },
-      width: {
+      weight: {
         type: Sequelize.FLOAT,
         allowNull: false,
       },
