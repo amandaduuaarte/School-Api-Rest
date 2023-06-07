b3e68e9fa81b6dc00c6927da430ab7b0ee55668b
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-06 21:17:13 -0300

    feat: Add more rules for file validation

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,6 +5,13 @@ const random = () => {
   Math.floor(Math.random() * 1000 + 1000);
 };
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      return cb(null, multer.MulterError('File is not a valid MIME type'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
