22e2c1aafafa304ff2ad1439563f1bb2d0760f4c
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-06 21:17:52 -0300

    chore: Move route validations for controller

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,8 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
 class PhotoController {
   async store(req, res) {
-    res.status(200).json(
-      req.file,
-    );
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import photoController from '../controllers/PhotoController';
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('photo'), photoController.store);
+router.post('/', photoController.store);
 
 export default router;
