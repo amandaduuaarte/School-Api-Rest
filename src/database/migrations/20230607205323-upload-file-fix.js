144bfe1e17547a802d770c99a14832bc321c95cb
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-10 18:14:17 -0300

    fix: Adjusting column names in photo table

---
+++ src/database/migrations/20230607205323-upload-file-fix.js
@@ -0,0 +1,43 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('photos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'students',
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('photos');
+  },
+};
