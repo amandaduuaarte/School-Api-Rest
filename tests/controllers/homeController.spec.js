34f7e5d6bb2269af113df6cb86b074d4bd46caaa
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-11 13:01:08 -0300

    chore: Remove test folders

--- tests/controllers/homeController.spec.js
+++
@@ -1,8 +0,0 @@
-import homeController from '../../src/controllers/HomeController';
-
-describe('Home Controller test', () => {
-  it('Should send 200 for successful response', () => {
-    const sendSuccessResponse = homeController.index;
-    // ToDo: Should send 200 for successful response when created new user
-  });
-});
--- tests/routes/home.spec.js
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import app from '../../app';
-
-describe('Home router test', () => {
-  it('Should return have any return in home router(get)', async () => {
-    const res = await request(app).get('/');
-    expect(res.status).toBe(200);
-  });
-});
