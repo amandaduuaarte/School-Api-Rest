e603db07429d0c4205ad317870fa91e677c17d11
amandaduuaarte <amandaduuaarte@gmail.com>
2023-06-24 17:34:07 -0300

    server

--- dist/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req, res) {
-    res.status(200).json(
-      'Index',
-    );
-  }
-}
-
-exports. default = new HomeController();
